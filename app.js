71be89e0264adc7fc9e80fb3aabadb9ab3625a50
Tayo <eyitt1@gmail.com>
2025-02-10 18:52:09 +0000

    First pass of getting CFN JSON

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
 
-const bucklerId = "";
-const bucklerRId = "";
-const urlToken = "";
-const league = "";
-const page = "";
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var url =`https://www.streetfighter.com/6/buckler/_next/data/${urlToken}/en/ranking/league.json?character_filter=1&character_id=luke&platform=1&user_status=1&home_filter=1&home_category_id=0&home_id=1&league_rank=${league}&page=${page}`;
+const bucklerId = process.env.BUCKLER_ID;
+const bucklerRId = process.env.BUCKLER_RID;
+const urlToken = process.env.URL_TOKEN;
+const league = "All";
+const page = "1";
+const referer = "https://www.streetfighter.com/6/buckler/ranking/league?character_filter=1&character_id=luke&platform=1&user_status=1&home_filter=1&home_category_id=0&home_id=1&league_rank=1&page=1";
 
-async function getCFNData(){
+var url =`https://www.streetfighter.com/6/buckler/_next/data/${urlToken}/en/ranking/league.json?`;
+
+async function getCFNData() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+
+      method: "GET",
+      headers:{
+        "Cookie": `buckler_r_id=${bucklerRId}; buckler_praise_date=1739206388785; buckler_id=${bucklerId}`,
+        "Referer": referer,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
+      }
+    }
+    );
     if (!response.ok) {
       return `Response status: ${response.status}`;
     }
@@ -24,8 +36,17 @@ async function getCFNData(){
   }
 }
 
-app.get('/', (req, res) => {
-  res.send(getCFNData())
+function filterData(data){
+  return data.league_point_ranking;
+}
+
+app.get('/', async (req, res) => {
+  const cfnData = await getCFNData();
+  console.log("Recieved a request for CFN data");
+  console.log("Printing CFN data");
+  console.log(cfnData);
+  const filteredData = filterData(cfnData.pageProps);
+  res.send(filteredData);
 })
 
 app.listen(port, () => {
